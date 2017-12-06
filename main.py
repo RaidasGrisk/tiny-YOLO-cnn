0be07eae34f6b7e2348b8dfd8760ca23f3d35884
Raidas Grisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-12-06 21:20:14 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,147 +1,60 @@
 """
+This is the code for running the whole tiny-yolo model.
 
+What's being done here:
+1. Open an image and reshape to (416, 416, 3)
+2. Forward propagate the image through tiny-yolo model
+1. Convert raw yolo output to readable form: boxes, scores, classes (and mark box corners for each box)
+2. Filter out boxes with low probability of present object
+3. Remove overlapping boxes using non-max-suppression
+4. Draw the result
 """
 
 from build_model import model
-from PIL import Image
+from utils import process_yolo_output, filter_yolo_boxes, non_max_suppression, object_list, colors
 import os
 import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+
+# initiate model and get output
+Model = model()
 
 # get input
-im = Image.open(os.getcwd() + '\\images\\image1.jpg')
-im = im.resize((416, 416), Image.ANTIALIAS)
-im.save('image0', 'JPEG')
-x = np.asarray(im).reshape(1, 416, 416, 3) / 255
+# TODO: specify input image path
+im = Image.open(os.getcwd() + '\\images\\image0.jpg')
+im_resized = im.resize((416, 416), Image.ANTIALIAS)
+yolo_input = np.asarray(im_resized).reshape(1, 416, 416, 3) / 255
 
-# get output
-Model = model()
-output = Model.predict(x)
+# forward-prop and process output
+yolo_output = Model.predict(yolo_input)
+boxes, scores, classes = process_yolo_output(yolo_output)
+boxes, scores, classes = filter_yolo_boxes(boxes, scores, classes, threshold=.4)
+boxes, scores, classes = non_max_suppression(boxes, scores, classes, threshold=0.9)
 
+# draw output
+draw = ImageDraw.Draw(im)
+width, height = im.size
+_, all_boxes, _ = np.shape(boxes)
+line_width = 5
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+for box in range(all_boxes):
 
+    # rescale boxes according ti size of original image
+    top, left, bottom, right = boxes[:, box, :]
+    top = int(top * height)
+    left = int(left * width)
+    bottom = int(bottom * height)
+    right = int(right * width)
 
-def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x), axis=4, keepdims=True)
+    # draw multiple lines to increase its width
+    for line in range(line_width):
+        draw.rectangle([left - 1 * line, top + 1 * line, right + 1 * line, bottom - 1 * line],
+                       fill=None, outline=colors[classes[box]])
 
+    draw.text((left, bottom), '{} {:.2f}'.format(object_list[classes[box]], scores[box]),
+              font=ImageFont.truetype('arial', 16), fill=colors[classes[box]])
 
-def process_yolo_output(model_output):
-    """
-    Converts raw yolo output to readable objects.
-    model_output shape (None, 13, 13, 425), (5+80)*5 == 425
-
-    Arguments:
-    model_output = final yolo layer
-
-    Returns:
-    boxes - box coordinates (x, y, w ,h)
-    scores - probability that object is present in each box (5 boxes)
-    classes - probability of each class present in a box (80 classes)
-
-    """
-
-    # prepare raw model output
-    b, h, w, num = np.shape(model_output)
-    model_output_reshaped = model_output.reshape(b, h, w, 5, 85)
-    scores = sigmoid(model_output_reshaped[..., 4:5])
-    boxes = model_output_reshaped[..., 0:4]
-    classes = softmax(model_output_reshaped[..., 5:])
-
-    return boxes, scores, classes
-
-
-def filter_boxes(boxes, scores, classes, threshold=0.6):
-    """
-    Filters boxes given probability threshold.
-
-    Arguments:
-    threshold is float, to filter boxes according to class prob
-
-    Returns:
-    Filtered boxes, scores, classes
-    """
-
-    # get a class for each box
-    box_scores = scores * classes
-    box_classes = np.argmax(box_scores, axis=-1)
-    box_class_scores = np.max(box_scores, axis=-1)
-
-    # apply threshold
-    score_mask = box_class_scores > threshold
-    boxes_filtered = boxes[score_mask]
-    scores_filtered = box_class_scores[score_mask]
-    classes_filtered = box_classes[score_mask]
-
-    return boxes_filtered, scores_filtered, classes_filtered
-
-
-def non_max_suppression(boxes, scores, classes, threshold=0.95):
-    """
-    Filter overlapping boxes.
-    Arguments: boxes, scores, classes, threshold
-    Returns: boxes, scores, classes
-
-    Source:
-    https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
-    """
-
-    x1 = np.array(boxes[..., 0])
-    y1 = np.array(boxes[..., 1])
-    x2 = np.array(boxes[..., 2])
-    y2 = np.array(boxes[..., 3])
-
-    area = (x2 - x1 + 1) * (y2 - y1 + 1)
-    idxs = np.argsort(y2)
-
-    pick = []
-    while len(idxs) > 0:
-
-        # grab the last index in the indexes list and add the
-        # index value to the list of picked indexes
-        last = len(idxs) - 1
-        i = idxs[last]
-        pick.append(i)
-
-        # find the largest (x, y) coordinates for the start of
-        # the bounding box and the smallest (x, y) coordinates
-        # for the end of the bounding box
-        xx1 = np.maximum(x1[i], x1[idxs[:last]])
-        yy1 = np.maximum(y1[i], y1[idxs[:last]])
-        xx2 = np.minimum(x2[i], x2[idxs[:last]])
-        yy2 = np.minimum(y2[i], y2[idxs[:last]])
-
-        # compute the width and height of the bounding box
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
-
-        # compute the ratio of overlap
-        overlap = (w * h) / area[idxs[:last]]
-
-        # delete all indexes that do not conform threshold
-        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > threshold)[0])))
-
-    return boxes[pick], scores[pick], classes[pick]
-
-
-# boxes, scores, classes = process_yolo_output(output)
-# boxes, scores, classes = filter_boxes(boxes, scores, classes, threshold=0.15)
-# boxes, scores, classes = non_max_suppression(boxes, scores, classes, threshold=0.95)
-#
-#
-# from PIL import Image, ImageFont, ImageDraw
-#
-# source_img = Image.open(os.getcwd() + '\\images\\image1.jpg')
-#
-# draw = ImageDraw.Draw(source_img)
-# draw.rectangle(((0, 00), (100, 100)), fill=None)
-# draw.text((20, 70), "something123", font=ImageFont.truetype('arial'))
-#
-# source_img.save('rect', "JPEG")
-#
-# xy = boxes[:, 0:2]
-# wh = boxes[:, 2:4]
-#
-#
-# left = (xy - (wh / 2.))
-# right =
+# TODO: specify output image path and name
+im.save('output_image', "JPEG")
+output_image = Image.open(os.getcwd() + '\\output_image')
+output_image.show()
---
+++ utils.py
@@ -0,0 +1,156 @@
+"""
+Helper functions for processing raw yolo output
+
+Much much help from:
+https://github.com/allanzelener/YAD2K/blob/master/yad2k/models/keras_yolo.py
+https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+"""
+
+import numpy as np
+import colorsys
+import random
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+def process_yolo_output(yolo_output):
+    """
+    Converts raw yolo output to readable objects.
+    model_output shape (None, 13, 13, 425), (5+80)*5 == 425
+
+    Arguments:
+    model_output = final yolo layer
+
+    Returns:
+    boxes - box corners (4, 13, 13, 5, 1) respectively (4 corners, box, box, anchors)
+    scores - probability that object is present in each box (13, 13, 5, 1)
+    classes - probability of each class present in a box (13, 13, 5, 80)
+    """
+
+    # get output dims
+    _, h, w, _ = np.shape(yolo_output)
+
+    # structure yolo output
+    yolo_output = np.reshape(yolo_output, [w, h, 5, 85])
+    box_xy = sigmoid(yolo_output[..., :2])
+    box_wh = np.exp(yolo_output[..., 2:4])
+    scores = sigmoid(yolo_output[..., 4:5])
+    classes = softmax(yolo_output[..., 5:])
+
+    # get conv index
+    conv_index = np.array([_ for _ in np.ndindex(w, h)])
+    conv_index = conv_index[:, [1, 0]]  # swap columns for YOLO ordering
+    conv_index = conv_index.reshape(1, h, w, 1, 2)
+
+    # reshape anchors (the anchors are taken from .cfg file)
+    anchors = [[0.57273, 0.677385], [1.87446, 2.06253], [3.33843, 5.47434], [7.88282, 3.52778], [9.77052, 9.16828]]
+    anchors_tensor = np.reshape(anchors, [1, 1, 1, 5, 2])
+
+    # get coordinates to each grid and anchor
+    box_xy = (box_xy + conv_index) / w
+    box_wh = box_wh * anchors_tensor / [w, h]
+
+    # get corners of each box
+    box_mins = box_xy - (box_wh / 2)
+    box_maxes = box_xy + (box_wh / 2)
+    boxes = np.concatenate([box_mins[..., 1:2], box_mins[..., 0:1], box_maxes[..., 1:2], box_maxes[..., 0:1]])
+
+    return boxes, scores, classes
+
+
+def filter_yolo_boxes(boxes, scores, classes, threshold=.7):
+    """
+    Filters boxes given probability threshold.
+
+    Arguments:
+    threshold is float, to filter boxes according to class prob
+
+    Returns:
+    Filtered boxes, scores, classes
+    """
+
+    # get a class for each box
+    box_scores = scores * classes
+    box_classes = np.argmax(box_scores, axis=-1)
+    box_class_scores = np.max(box_scores, axis=-1)
+
+    # apply threshold
+    score_mask = box_class_scores > threshold
+    boxes = boxes[:, score_mask, :]
+    scores = box_class_scores[score_mask]
+    classes = box_classes[score_mask]
+    
+    return boxes, scores, classes
+
+
+def non_max_suppression(boxes, scores, classes, threshold=0.99):
+    """
+    Filter overlapping boxes.
+    Arguments: boxes, scores, classes, threshold
+    Returns: boxes, scores, classes
+
+    Source:
+    https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+    """
+
+    x1 = np.array(boxes[0, ...])
+    y1 = np.array(boxes[1, ...])
+    x2 = np.array(boxes[2, ...])
+    y2 = np.array(boxes[3, ...])
+
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    indexes = np.argsort([item[0] for item in y2])
+
+    pick = []
+    while len(indexes) > 0:
+
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(indexes) - 1
+        i = indexes[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[indexes[:last]])
+        yy1 = np.maximum(y1[i], y1[indexes[:last]])
+        xx2 = np.minimum(x2[i], x2[indexes[:last]])
+        yy2 = np.minimum(y2[i], y2[indexes[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[indexes[:last]]
+
+        # delete all indexes that do not conform threshold
+        indexes = np.delete(indexes, np.concatenate(([last], np.where(overlap > threshold)[0])))
+
+    return boxes[:, pick, :], scores[pick], classes[pick]
+
+
+object_list = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+               'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+               'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+               'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa',
+               'pottedplant', 'bed', 'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard',
+               'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+               'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+# Generate colors for drawing bounding boxes
+# https://github.com/allanzelener/YAD2K/blob/master/test_yolo.py
+hsv_tuples = [(x / 80, 1., 1.) for x in range(80)]
+colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+random.seed(2)  # Fixed seed for consistent colors across runs
+random.shuffle(colors)  # Shuffle colors to de-correlate adjacent classes
+random.seed(None)  # Reset seed to default
